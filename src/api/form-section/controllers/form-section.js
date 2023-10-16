0c4aace88ca1d412fae4baf1cb9a272f705dfe4b
Sagato <danyal.iqbal@cheveo.de>
2023-10-16 14:33:51 +0200

    feat(strapi): added form_section.

---
+++ src/api/form-section/controllers/form-section.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * form-section controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::form-section.form-section');
---
+++ src/api/form-section/routes/form-section.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * form-section router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::form-section.form-section');
